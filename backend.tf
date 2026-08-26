aec2b3d2763a9e75c0c4ec0b3293a03ac501405e
Dishant Saini <dishantsaini203@gmail.com>
2026-08-26 15:59:02 +0530

    Clean up backend.tf by removing comments
    Removed commented lines for DynamoDB table and encryption.

--- backend.tf
+++ backend.tf
@@ -3,9 +3,7 @@ terraform {
     bucket = "vpc-terraform-state-file-bucket"
     key    = "eks-prod/terraform.tfstate"
     region = "us-east-1"
-    # 4. The DynamoDB table for locking
-    # dynamodb_table = "terraform-lock-table"
-    # 5. Encrypt state at rest
+  
     encrypt = true
   }
 }
